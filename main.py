c8825a0286b1ab73064452b32fd3eecf05f24906
Nathan-Busse <bussenathan@icloud.com>
2025-09-28 14:59:52 +0100

    most stable build to rollback toogit add *!

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ PROCESSES = [
     
     # 4. Web UI Server (Starts Flask server, reads from logger JSON files)
 #    ("Web Server", "web_ui/app_server.py"),
-# ]
+]
 
 def launch_processes():
     """Launches all configured processes concurrently using subprocess.Popen."""
