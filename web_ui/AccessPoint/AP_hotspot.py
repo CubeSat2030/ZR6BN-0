68074373ce73e8a12d2590f56cdd332fd2d539c0
Nathan Graham Busse <bussenathan@icloud.com>
2025-10-01 16:34:02 +0200

    Create AP_hotspot.py

---
+++ web_ui/AccessPoint/AP_hotspot.py
@@ -0,0 +1,6 @@
+# Creates a hotspot AP using the Raspberry pi Zero w's built-in antenna to allow devices to access kabot-1.local's web ui.
+
+# TODO:
+# Check if the pi is connected to a network.
+# if the pi is connected to a network keep the hotspot off
+# if the pi is not connected to a network the hotspot will activate
