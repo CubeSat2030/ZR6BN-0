b68bb0dd5e7a46a6877a52a594765869542c6294
Nathan Graham Busse <bussenathan@icloud.com>
2025-10-01 21:53:11 +0200

    Delete web_ui/AccessPoint directory

--- web_ui/AccessPoint/AP_hotspot.py
+++
@@ -1,4 +0,0 @@
-# Creates a hotspot AP using the Raspberry pi Zero w's built-in antenna to allow devices to access kabot-1.local's web ui.
-
-# TODO:
-# Host a low power/ram/cpu consuming AP hotspot that client devices can connect too...
