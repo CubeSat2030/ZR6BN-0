77b3f413ecd8c262f18dde57f2cba5062579c25b
Nathan Graham Busse <bussenathan@icloud.com>
2025-10-02 08:44:40 +0200

    Update app_server.py

--- web_ui/app_server.py
+++ web_ui/app_server.py
@@ -517,4 +517,4 @@ if __name__ == "__main__":
     else:
         print("Buzzer Countdown: INACTIVE (gpiozero not found or failed to initialize).")
     print("------------------------------------------------------------------")
-    app.run(host="0.0.0.0", port=5000, debug=False)
+    app.run(host="192.168.50.1", port=5000, debug=False) # binds the bluetooth pan
