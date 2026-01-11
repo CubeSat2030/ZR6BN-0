7592ec50ba57f5e51444bde9df9a300be30e7f0e
Nathan Graham Busse <bussenathan@icloud.com>
2026-01-11 20:50:12 +0200

    Fix indentation and add missing closing braces

--- web_ui/app_server.py
+++ web_ui/app_server.py
@@ -88,7 +88,8 @@ SCRIPTS_CONFIG = {
         "title": "Payload Flight Simulation",
         "sim_script": SIM_DIR / "payload_flight_simulation.py",
         "video_file": "payload_flight_simulation.mp4"
-    },
+    }
+        },
     "sound": {
         "title": "Sound Logger",
         "log_script": LOG_DIR / "sound_logger.py",
